d9c69da97e95540c162e730bf6b5cabf2fb0629e
g-l-y-k <glyk847@gmail.com>
2024-03-03 19:53:22 +0300

    23 и 24

--- test/test_group_add_xlsx.py
+++ test/test_group_add_xlsx.py
@@ -5,13 +5,13 @@ def prepare_new_data():
 
 
 def test_add_group(app, xlsx_data):
-    print(xlsx_data)
-    # # generator.groups.new_data_for_test()
-    # old_list = app.groups.get_groups_list()
-    # app.groups.add_new_group(xlsx_data[0])
-    # new_list = app.groups.get_groups_list()
-    # old_list.append(xlsx_data[0])
-    # assert sorted(new_list) == sorted(old_list)
+    # print(xlsx_data)
+    # generator.groups.new_data_for_test()
+    old_list = app.groups.get_groups_list()
+    app.groups.add_new_group(xlsx_data[0])
+    new_list = app.groups.get_groups_list()
+    old_list.append(xlsx_data[0])
+    assert sorted(new_list) == sorted(old_list)
 
 
 if __name__ == '__main__':
